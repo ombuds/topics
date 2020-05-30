53c341ac317261918221d3b55347590621af6338
ombuds <ombudsthom@gmail.com>
2020-05-30 11:50:48 -0400

    Initial commit from cookiecutter data-science template.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Document classification model and service built using the Yahoo! Question-Answer dataset.',
+    author='Thomas Reid',
+    license='',
+)
